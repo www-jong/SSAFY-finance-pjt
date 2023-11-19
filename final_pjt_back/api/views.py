f55a2fb8593a9a4ff799ed64e46ab2e0ae5aea03
Minjin <tjmg726@gmail.com>
2023-11-20 00:26:14 +0900

    회원가입 페이지 상단 마진 수정, 패스워드 경고 메시지 작업

--- final_pjt_back/api/views.py
+++ final_pjt_back/api/views.py
@@ -113,6 +113,8 @@ def exchange_v2(request):
                     latest_exchange_date=ExchangeDate.objects.latest('date_info')
                     exchange_info_list = ExchangeInfo.objects.filter(exchangeDate=latest_exchange_date)
                     data = ExchangeInfoSerializer(exchange_info_list, many=True)
+                    if not data:
+                        data={'nodata'}
                     return Response({'message': '영업시간이 아닙니다','data':data.data,'datetime':'latest'})
                 for item in data:
                     item.pop('result',None)
